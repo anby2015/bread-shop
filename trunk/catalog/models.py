2be3890644d70079f489207e284e81d476785114
lirui.zhu@gmail.com <lirui.zhu@gmail.com@7cafdc97-f9f6-f0c8-c3cc-7ec76b52ffe6>
2011-06-23 21:40:58 +0000

    Initial import.

---
+++ trunk/catalog/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    description = models.TextField()
+
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "products"
+        ordering = ['-created_at']
+
+    def __unicode__(self):
+            return self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('catalog_product', (), {'product_slug': self.slug})
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    description = models.TextField()
+
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "categories"
+        ordering = ['-created_at']
+
+    def __unicode__(self):
+            return self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('catalog_category', (), {'category_slug': self.slug})
