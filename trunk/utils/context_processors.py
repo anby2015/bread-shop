13ad41dae85fd298fa36e6f8f09f8f30ba58df2a
lirui.zhu@gmail.com <lirui.zhu@gmail.com@7cafdc97-f9f6-f0c8-c3cc-7ec76b52ffe6>
2011-06-24 03:12:01 +0000

    Add utils app. It includes context processor.

---
+++ trunk/utils/context_processors.py
@@ -0,0 +1,13 @@
+import settings
+
+__author__ = 'leroy'
+
+def bread(request):
+    """ context processor for the site templates """
+    return {
+        'site_name': settings.SITE_NAME,
+        'meta_keywords': settings.META_KEYWORDS,
+        'meta_description': settings.META_DESCRIPTION,
+#        'analytics_tracking_id': settings.ANALYTICS_TRACKING_ID,
+        'request': request
+    }
