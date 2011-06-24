4cf697eb5c6195c4fbeb67c2f069f45d11ac61c5
lirui.zhu@gmail.com <lirui.zhu@gmail.com@7cafdc97-f9f6-f0c8-c3cc-7ec76b52ffe6>
2011-06-24 04:57:41 +0000

    Add user predefine shopping cart to cart app.

--- trunk/cart/models.py
+++ trunk/cart/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -34,3 +35,37 @@ class CartItem(models.Model):
         """ called when a POST request comes in for a Product instance already in the shopping cart """
         self.quantity = self.quantity + int(quantity)
         self.save()
+
+
+class UserPredefinedCart(models.Model):
+
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True, help_text='Unique value for product page URL, created automatically from name.')
+
+    description = models.TextField()
+    is_default = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(User, null=True)
+
+    class Meta:
+        db_table = 'cart_user_predefined_cart'
+        ordering = ['-created_at', 'user']
+        verbose_name_plural = 'User Predefined Shopping Cart'
+
+    def __unicode__(self):
+        return self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('cart_user_defined', (), { 'product_slug': self.slug })
+
+
+class UserPreSelectCartItem(models.Model):
+
+    quantity = models.IntegerField(default=1)
+    product = models.ForeignKey(Product, unique=False)
+
+    cart = models.ForeignKey(UserPredefinedCart, null=False)
