fd3fa4904f9e095792f1ac66945cf1d99151f8b2
lirui.zhu@gmail.com <lirui.zhu@gmail.com@7cafdc97-f9f6-f0c8-c3cc-7ec76b52ffe6>
2011-06-24 02:58:48 +0000

    Add basic view setting for catalog app.

--- trunk/catalog/views.py
+++ trunk/catalog/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+
+def index(request, template_name="catalog/index.html"):
+    page_title = 'Welcome | Order System'
+    return render_to_response(template_name, locals(), context_instance=RequestContext(request))
