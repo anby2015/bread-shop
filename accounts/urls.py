2ab57ee0a3c5ff99b9eb3f9a00be6faac60c3b29
lirui.zhu <lirui.zhu@gmail.com>
2011-06-27 11:57:56 +0000

    Add change password and edit billing info

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,10 +6,8 @@ __author__ = 'leroy'
 urlpatterns = patterns('bread.accounts.views',
 	url(r'^register/$', 'register', {'template_name': 'registration/register.html', 'SSL': settings.ENABLE_SSL }, 'register'),
 	url(r'^my_account/$', 'my_account', {'template_name': 'registration/my_account.html'}, 'my_account'),
-#	(r'^order_info/$', 'order_info',
-#	 	{'template_name': 'registration/order_info.html'}, 'order_info'),
-#	(r'^order_details/(?P<order_id>[-\w]+)/$', 'order_details',
-#	 	{'template_name': 'registration/order_details.html'}, 'order_details'),
+	url(r'^order_info/$', 'order_info', {'template_name': 'registration/order_info.html'}, 'order_info'),
+#	url(r'^order_details/(?P<order_id>[-\w]+)/$', 'order_details', {'template_name': 'registration/order_details.html'}, 'order_details'),
 )
 
 urlpatterns += patterns('django.contrib.auth.views',
